18a9f393c3df6c8896fcbf2602b09123162c79cc
BumblebeeCode0101 <janik1113@icloud.com>
2024-10-28 21:09:26 +0100

    Fixed redirect

--- auth_check.php
+++ auth_check.php
@@ -2,7 +2,7 @@
 session_start();
 
 if(!isset($_SESSION['username'])) {
-    header("location: http://localhost/technoweb/login-user.php");
+    header("location: login-user.php");
     exit();
 }
 ?>
